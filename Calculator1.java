5a4c8ff454392b1a7ca514e3e7ae42773e8e725a
Prajakta Garad <garadprajkta29@gmail.com>
2022-07-01 21:34:54 +0530

    File has been commited

---
+++ Calculator1.java
@@ -0,0 +1,21 @@
+
+public class Calculator1 {
+	public static void main(String[] args) {
+		
+		int num1=200, num2=400;
+		
+		int sum = num1+num2;
+		System.out.println("Addition ="+sum );
+		
+		int sub = num1-num2;
+		System.out.println("Substraction ="+sub);
+		
+		int mul = num1*num2;
+		System.out.println("Multiplication ="+mul);
+		
+		int div = num1/num2;
+		System.out.println("Division ="+ div);
+		
+	}
+
+}
